d1fc63ea654fa5fd7f4e9258012352dc860f4577
Verley Antoine <antoine.verley@hotmail.fr>
2016-04-27 18:20:45 +0200

    bernoulli modele commentaires

--- src/bernoulli.c
+++ src/bernoulli.c
@@ -9,6 +9,7 @@ void remplirListeProbaOcurence(struct probaOccurence* listeProba, struct documen
 
   while( courMot != NULL ){
     
+    //Si la liste des probas est vide
     if (courProba == NULL){
       courProba = malloc(sizeof(struct probaOccurence));
       courProba->indice = courMot->indice;
@@ -16,8 +17,12 @@ void remplirListeProbaOcurence(struct probaOccurence* listeProba, struct documen
       courProba->suivante = NULL;
     }
     else{
+      //Si le terme a déjà été observe dans un document de la classe
+      //On incremente l'occurence observée
       if( courProba->indice ==  courMot->indice ) (courProba->proba)++;
       else{
+	//Si l'indice du terme est superieur à tous les indices des termes 
+	//precedemment relevés, on ajoute en tete sa proba d'occurence
 	if ( courProba->indice < courMot->indice ){
 	  struct probaOccurence* nouvelleTete = malloc(sizeof(struct probaOccurence));
 	  nouvelleTete->indice = courMot->indice;
@@ -27,6 +32,8 @@ void remplirListeProbaOcurence(struct probaOccurence* listeProba, struct documen
 	  courProba = listeProba;
 	}
 	else{
+	  //Si le terme n'a pas ete observé et que son indice est inferieur 
+	  //à tous les autres observés, on l'insere en queue
 	  if (courProba->suivante == NULL ){
 	    struct probaOccurence* nouvelleQueue = malloc(sizeof(struct probaOccurence));
 	    nouvelleQueue->indice = courMot->indice;
@@ -36,6 +43,8 @@ void remplirListeProbaOcurence(struct probaOccurence* listeProba, struct documen
 	    courProba = nouvelleQueue;
 	  }
 	  else{
+	    //Si l'indice du terme est compris entre les indices des termes 
+	    //courant et suivant, on l'insere entre les deux
 	    if (courProba->suivante->indice < courMot->indice){
 	      struct probaOccurence* nouvelleProba = malloc(sizeof(struct probaOccurence));
 	    nouvelleProba->indice = courMot->indice;
@@ -47,6 +56,8 @@ void remplirListeProbaOcurence(struct probaOccurence* listeProba, struct documen
 	    }
 	    
 	    else{
+	      //Sinon on va en queue ou jusqu'à se retrouver entre deux indices 
+	      //encadrant l'indice actuel
 	      while( (courProba->suivante != NULL) && (courProba->suivante->indice > courMot->indice) ) {courProba = courProba->suivante;}
 	      if (courProba->suivante == NULL){
 		struct probaOccurence* nouvelleQueue = malloc(sizeof(struct probaOccurence));
@@ -57,12 +68,21 @@ void remplirListeProbaOcurence(struct probaOccurence* listeProba, struct documen
 		courProba = nouvelleQueue;
 	      }
 	      else{
-		struct probaOccurence* nouvelleProba = malloc(sizeof(struct probaOccurence));
-		nouvelleProba->indice = courMot->indice;
-		nouvelleProba->proba = 1;
-		nouvelleProba->suivante = courProba->suivante;
-		courProba->suivante = nouvelleProba;
-		courProba = nouvelleProba;
+		//l'indice suivant est egal à l'indice courant
+		//On incremente l'occurence observee
+		if (courProba->suivante->indice == courMot->indice){
+		  courProba = courProba->suivante;
+		  (courProba->proba)++;
+		}
+		else{
+		  //sinon, encadrement strict et on insere
+		  struct probaOccurence* nouvelleProba = malloc(sizeof(struct probaOccurence));
+		  nouvelleProba->indice = courMot->indice;
+		  nouvelleProba->proba = 1;
+		  nouvelleProba->suivante = courProba->suivante;
+		  courProba->suivante = nouvelleProba;
+		  courProba = nouvelleProba;
+		}
 	      }
 	    }
 	  }
@@ -79,14 +99,20 @@ void remplirListeProbaOcurence(struct probaOccurence* listeProba, struct documen
 
 double determinerParametreClasse(int classe, struct probaOccurence* listeProba, struct document* ensemble_documents,int nbDocuments){
 
+  //probabilite qu'un document soit de la classe etudiee
   double probaClasse = 0; 
 
   struct document* cour = ensemble_documents;
 
+  //parcours de l'ensemble des documents
   while ( cour != NULL ){
 
+    //si le document courant est de la classe à étudier
     if(cour->categorie == classe){
+      //comptage du nombre de docs dans classe
       probaClasse++;
+      //Modification de la liste des probabilites d'occurrence des 
+      //mots dans la classe avec le contenu du doc courant
       remplirListeProbaOcurence(listeProba, cour);      
     }
 
@@ -97,6 +123,8 @@ double determinerParametreClasse(int classe, struct probaOccurence* listeProba, 
 
   struct probaOccurence* courProba = listeProba;
   
+  //reparcours de la liste des probabilites pour les calculer
+  //avec le nombre de documents de docs de la classe  
   while (courProba != NULL){
 
     courProba->proba = (courProba->proba + 1)/(probaClasse + 2);
@@ -113,11 +141,16 @@ double determinerParametreClasse(int classe, struct probaOccurence* listeProba, 
 
 struct modeleBernoulli* determinerParametreModeleBernoulli(int nbClasses, struct document* ensemble_documents, int nbDocuments){
 
+  //tableau de la distribution des classes
   double* tabProbaClasses = malloc( nbClasses*sizeof(double) );
+  //tableau des probabilites d'occurence des termes pour chaque classe
   struct probaOccurence** tabProbaOccurences = malloc(nbClasses*sizeof(struct probaOccurence*));
+
   int i = 0;
   for( i = 0; i < nbClasses; i++ ){
     tabProbaOccurences[i] = NULL;
+    //Calcul de la probabilite d'appartenannce à classe i
+    //et construction liste probas d'occurence pour classe i
     tabProbaClasses[i] = determinerParametreClasse(i+1, tabProbaOccurences[i], ensemble_documents, nbDocuments);
   }
 
--- src/bernoulli.h
+++ src/bernoulli.h
@@ -11,15 +11,21 @@ struct probaOccurence{
 
 struct modeleBernoulli;
 struct modeleBernoulli{
+  //tableau de la distribution des classes
   double* tabProbaClasse;
+  //tableau des probabilites d'occurence des termes pour chaque classe
   struct probaOccurence** tabProbaOccurence;
 };
 
-
+//Reporter les occurences des termes dans doc au sein de listeProba, liste des
+//probas d'occurence des termes dans les docs d'une meme classe
 void  remplirListeProbaOcurence(struct probaOccurence* listeProba, struct document* doc);
 
+//Calculer la probabilite empirique de la classe et construire listeProba avec les probabilites 
+//d'occurence des termes pour un doc de classe
 double determinerParametreClasse(int classe, struct probaOccurence* listeProba, struct document* ensemble_documents, int nbDocuments);
 
+//Calculer les parametres du modele de Bernoulli à partir des données d'entrée
 struct modeleBernoulli* determinerParametreModeleBernoulli(int nbClasses, struct document* ensemble_documents, int nbDocuments); 
 
 
